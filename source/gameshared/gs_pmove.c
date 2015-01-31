af6f659a955306c7d10599c0ded1164388618384
Karl Terävä <karl@terava.se>
2015-01-31 02:35:34 +0100

    Reset preshot based on a timer (2 seconds)

--- source/gameshared/gs_pmove.c
+++ source/gameshared/gs_pmove.c
@@ -789,7 +789,6 @@ static void PM_Move( void )
 		float hspeed = VectorLengthFast( tv( pml.velocity[0], pml.velocity[1], 0 ) );
 		if( hspeed < DEFAULT_PLAYERSPEED_RACE + 5.0f )
 			RS_ResetPjState( pm->playerState->playerNum );
-			RS_ResetPsState( pm->playerState->playerNum );
 		// !racesow
 
 		if( !pml.velocity[0] && !pml.velocity[1] )
