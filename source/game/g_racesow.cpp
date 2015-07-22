b8074e9c90087f4c914655b1d9d4efbee77be292
Mark Moes <mark_moes@live.nl>
2015-07-22 22:47:13 +0200

    g_racesow.cpp: finalize weapondefs

--- source/game/g_racesow.cpp
+++ source/game/g_racesow.cpp
@@ -35,28 +35,28 @@ cvar_t *rs_gunblade_splashfrac;
 void RS_Init( void )
 {
 	rs_grenade_minKnockback = trap_Cvar_Get( "rs_grenade_minKnockback", "1", CVAR_ARCHIVE );
-	rs_grenade_maxKnockback = trap_Cvar_Get( "rs_grenade_maxKnockback", "115", CVAR_ARCHIVE );
+	rs_grenade_maxKnockback = trap_Cvar_Get( "rs_grenade_maxKnockback", "120", CVAR_ARCHIVE );
 	rs_grenade_splash = trap_Cvar_Get( "rs_grenade_splash", "170", CVAR_ARCHIVE );
-	rs_grenade_speed = trap_Cvar_Get( "rs_grenade_speed", "820", CVAR_ARCHIVE );
+	rs_grenade_speed = trap_Cvar_Get( "rs_grenade_speed", "800", CVAR_ARCHIVE );
 	rs_grenade_timeout = trap_Cvar_Get( "rs_grenade_timeout", "1650", CVAR_ARCHIVE );
 	rs_grenade_gravity = trap_Cvar_Get( "rs_grenade_gravity", "1.22", CVAR_ARCHIVE );
-	rs_grenade_friction = trap_Cvar_Get( "rs_grenade_friction", "0.95", CVAR_ARCHIVE );
+	rs_grenade_friction = trap_Cvar_Get( "rs_grenade_friction", "0.85", CVAR_ARCHIVE );
 	rs_grenade_prestep = trap_Cvar_Get( "rs_grenade_prestep", "24", CVAR_ARCHIVE );
 	rs_grenade_splashfrac = trap_Cvar_Get( "rs_grenade_splashfrac", "2.5", CVAR_ARCHIVE );
 	rs_rocket_minKnockback = trap_Cvar_Get( "rs_rocket_minKnockback", "1", CVAR_ARCHIVE );
-	rs_rocket_maxKnockback = trap_Cvar_Get( "rs_rocket_maxKnockback", "94", CVAR_ARCHIVE );
-	rs_rocket_splash = trap_Cvar_Get( "rs_rocket_splash", "150", CVAR_ARCHIVE );
+	rs_rocket_maxKnockback = trap_Cvar_Get( "rs_rocket_maxKnockback", "108", CVAR_ARCHIVE );
+	rs_rocket_splash = trap_Cvar_Get( "rs_rocket_splash", "120", CVAR_ARCHIVE );
 	rs_rocket_speed = trap_Cvar_Get( "rs_rocket_speed", "950", CVAR_ARCHIVE );
 	rs_rocket_prestep = trap_Cvar_Get( "rs_rocket_prestep", "10", CVAR_ARCHIVE );
 	rs_rocket_antilag = trap_Cvar_Get( "rs_rocket_antilag", "0", CVAR_ARCHIVE );
-	rs_rocket_splashfrac = trap_Cvar_Get( "rs_rocket_splashfrac", "1.3", CVAR_ARCHIVE );
+	rs_rocket_splashfrac = trap_Cvar_Get( "rs_rocket_splashfrac", "1", CVAR_ARCHIVE );
 	rs_plasma_minKnockback = trap_Cvar_Get( "rs_plasma_minKnockback", "1", CVAR_ARCHIVE );
-	rs_plasma_maxKnockback = trap_Cvar_Get( "rs_plasma_maxKnockback", "22", CVAR_ARCHIVE );
+	rs_plasma_maxKnockback = trap_Cvar_Get( "rs_plasma_maxKnockback", "24", CVAR_ARCHIVE );
 	rs_plasma_splash = trap_Cvar_Get( "rs_plasma_splash", "40", CVAR_ARCHIVE );
 	rs_plasma_speed = trap_Cvar_Get( "rs_plasma_speed", "1700", CVAR_ARCHIVE );
-	rs_plasma_prestep = trap_Cvar_Get( "rs_plasma_prestep", "32", CVAR_ARCHIVE );
+	rs_plasma_prestep = trap_Cvar_Get( "rs_plasma_prestep", "0", CVAR_ARCHIVE );
 	rs_plasma_hack = trap_Cvar_Get( "rs_plasma_hack", "1", CVAR_ARCHIVE );
-	rs_plasma_splashfrac = trap_Cvar_Get( "rs_plasma_splashfrac", "1.15", CVAR_ARCHIVE );
+	rs_plasma_splashfrac = trap_Cvar_Get( "rs_plasma_splashfrac", "1", CVAR_ARCHIVE );
 	rs_gunblade_minKnockback = trap_Cvar_Get( "rs_gunblade_minKnockback", "10", CVAR_ARCHIVE ); // TODO: decide gunblade values
 	rs_gunblade_maxKnockback = trap_Cvar_Get( "rs_gunblade_maxKnockback", "60", CVAR_ARCHIVE );
 	rs_gunblade_splash = trap_Cvar_Get( "rs_gunblade_splash", "80", CVAR_ARCHIVE );
